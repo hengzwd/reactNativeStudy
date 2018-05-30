8c78c2ea06523223ac9241b8c6720850a0d3865d
hengzwd <616019420@qq.com>
2018-05-30 16:49:48 +0800

    rn

---
+++ app/components/NotifyPage.js
@@ -0,0 +1,157 @@
+/**
+ * Created by guoshuyu on 2017/11/10.
+ */
+
+import React, {Component, PureComponent} from 'react';
+import {
+    View, InteractionManager, StatusBar, Dimensions, StyleSheet
+} from 'react-native';
+import {Actions, Tabs} from 'react-native-router-flux';
+import styles from "../style"
+import * as Constant from "../style/constant"
+import I18n from '../style/i18n'
+import userActions from '../store/actions/user'
+import WebComponent from './widget/CustomWebComponent'
+import CommonBottomBar from './common/CommonBottomBar'
+import IssueListPage from './RepositoryIssueListPage'
+import RepositoryDetailActivity from './RepositoryDetailActivityPage'
+import {TabViewAnimated, TabBar, SceneMap} from 'react-native-tab-view';
+import ListPage from "./ListPage";
+
+/**
+ * 通知页面
+ */
+class NotifyPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.page = 2;
+        this._refresh = this._refresh.bind(this);
+        this._asRead = this._asRead.bind(this);
+        this._renderScene = this._renderScene.bind(this);
+        this.state = {
+            index: 0,
+            routes: [
+                {key: '1', title: I18n('notifyUnread')},
+                {key: '2', title: I18n('notifyParticipating')},
+                {key: '3', title: I18n('notifyAll')},
+            ],
+        }
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+        this.props.backNotifyCall && this.props.backNotifyCall()
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps && newProps.type === "allRead") {
+            newProps.type = "";
+            this._refresh();
+        }
+    }
+
+    _refresh() {
+        if (this.unReadList)
+            this.unReadList._refresh();
+        if (this.partList)
+            this.partList._refresh();
+        if (this.allList)
+            this.allList._refresh();
+    }
+
+    _handleIndexChange = index => this.setState({index});
+
+
+    _asRead(id) {
+        userActions.setNotificationAsRead(id).then(() => {
+            this._refresh();
+        })
+    }
+
+    _renderHeader = props =>
+        <TabBar {...props}
+                style={{backgroundColor: Constant.primaryColor}}
+                labelStyle={{color: Constant.white}}
+                indicatorStyle={{backgroundColor: Constant.miWhite}}
+        />;
+
+    _renderScene = ({route}) => {
+        switch (route.key) {
+            case '1':
+                return (
+                    <ListPage
+                        ref={(ref) => {
+                            this.unReadList = ref;
+                        }}
+                        dataType={'notify'}
+                        showType={'notify'}
+                        onItemClickEx={this._asRead}
+                        currentUser={this.props.ownerName}
+                        currentRepository={this.props.repositoryName}
+                    />
+                );
+            case '2':
+                return (
+                    <ListPage
+                        ref={(ref) => {
+                            this.partList = ref;
+                        }}
+                        dataType={'notify'}
+                        showType={'notify'}
+                        onItemClickEx={this._asRead}
+                        participating={true}
+                        currentUser={this.props.ownerName}
+                        currentRepository={this.props.repositoryName}
+                    />
+                );
+            case '3':
+                return (
+                    <ListPage
+                        ref={(ref) => {
+                            this.allList = ref;
+                        }}
+                        dataType={'notify'}
+                        showType={'notify'}
+                        onItemClickEx={this._asRead}
+                        all={true}
+                        currentUser={this.props.ownerName}
+                        currentRepository={this.props.repositoryName}
+                    />
+                );
+            default:
+                return null;
+        }
+    };
+
+
+    render() {
+        return (
+            <View style={styles.mainBox}>
+                <StatusBar hidden={false} backgroundColor={'transparent'} translucent barStyle={'light-content'}/>
+                <TabViewAnimated
+                    style={{
+                        flex: 1,
+                    }}
+                    lazy={true}
+                    swipeEnabled={false}
+                    navigationState={this.state}
+                    renderScene={this._renderScene.bind(this)}
+                    renderHeader={this._renderHeader}
+                    onIndexChange={this._handleIndexChange}
+                    initialLayout={{
+                        height: 0,
+                        width: Dimensions.get('window').width,
+                    }}
+                />
+            </View>
+        )
+    }
+}
+
+NotifyPage.defaultProps = {};
+
+
+export default NotifyPage
---
+++ app/components/widget/PullLoadMoreListView.js
@@ -0,0 +1,230 @@
+import React, {
+    Component,
+} from 'react';
+import PropTypes from 'prop-types';
+import {
+    View, Text, ListView, RefreshControl, ActivityIndicator, Platform, TouchableOpacity, ScrollView, Image
+} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import styles, {screenHeight, screenWidth} from "../../style"
+import * as Constant from "../../style/constant"
+import I18n from '../../style/i18n'
+import * as Config from '../../config'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+/**
+ * 上下拉列表控件
+ */
+class PullLoadMoreListView extends Component {
+
+    constructor(props) {
+        super(props);
+        this._renderFooter = this._renderFooter.bind(this);
+        this._refresh = this._refresh.bind(this);
+        this._loadMore = this._loadMore.bind(this);
+        //设置state
+        this.state = {
+            isRefresh: false,
+            isRefreshing: false,
+            showLoadMore: false,
+            showRefresh: true,
+        };
+        this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    }
+
+
+    /**
+     * 绘制load more footer
+     * */
+    _renderFooter() {
+
+        let footer = (this.state.showLoadMore) ?
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <ActivityIndicator
+                    color={Constant.primaryColor}
+                    animating={true}
+                    style={{height: 50}}
+                    size="large"/>
+                <Text style={{fontSize: 15, color: 'black'}}>
+                    {I18n('loadMoreing')}
+                </Text>
+            </View> : <View style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Text style={{fontSize: 15, color: 'black', margin: Constant.normalMarginEdge}}>
+                    {this.props.dataSource.length > 0 ? I18n('loadMoreEnd') : " "}
+                </Text>
+            </View>;
+
+        return (footer);
+    }
+
+    /**
+     * 刷新
+     * */
+    _refresh() {
+        if (this.state.isRefreshing) {
+            return
+        }
+        this.setState({
+            isRefreshing: true,
+            showLoadMore: false,
+            isRefresh: true,
+        });
+        let {eventAction} = this.props;
+        this.props.refresh && this.props.refresh();
+    }
+
+    /**
+     * 加载更多
+     * */
+    _loadMore() {
+        if (this.state.isRefreshing) {
+            return
+        }
+        if (!this.state.showLoadMore) {
+            return
+        }
+        if (this.props.dataSource.length === 0) {
+            return
+        }
+        this.setState({
+            isRefreshing: true,
+            showRefresh: false,
+        });
+        this.props.loadMore && this.props.loadMore();
+    }
+
+    render() {
+        let dataList = this.ds.cloneWithRows(this.props.dataSource);
+        let refreshProps = {
+            style: [styles.centered],
+            enable: (this.state.showRefresh && this.props.enableRefresh),
+            refreshing: this.state.isRefresh,
+            onRefresh: this._refresh,
+            tintColor: Constant.primaryColor,
+            title: I18n('refreshing'),
+            colors: [Constant.primaryColor, Constant.primaryLightColor],
+        };
+        if ((!this.props.dataSource || this.props.dataSource.length === 0)) {
+            if (!this.props.hasOwnProperty("renderHeader")) {
+                if (Platform.OS === 'android') {
+                    return (
+                        <RefreshControl
+                            {...refreshProps}
+                            style={[styles.centered, styles.flex]}>
+                            <TouchableOpacity style={[styles.centered, styles.absoluteFull]}
+                                              onPress={() => {
+                                                  this._refresh();
+                                                  this.showRefreshState();
+                                              }}>
+                                <Image source={require("../../img/logo.png")}
+                                       resizeMode={"contain"}
+                                       style={{width: 80, height: 80}}/>
+                                <Text style={[styles.normalText]}>
+                                    {I18n("listEmpty")}
+                                </Text>
+                            </TouchableOpacity>
+                        </RefreshControl>);
+                } else {
+                    return (
+                        <View style={[styles.centered, styles.flex]}>
+                            <TouchableOpacity style={[styles.centered, styles.absoluteFull, {zIndex: -999}]}
+                                              onPress={() => {
+                                                  this._refresh();
+                                                  this.showRefreshState();
+                                              }}>
+                                <Image source={require("../../img/logo.png")}
+                                       resizeMode={"contain"}
+                                       style={{width: 80, height: 80}}/>
+                                <Text style={[styles.normalText]}>
+                                    {I18n("listEmpty")}
+                                </Text>
+                            </TouchableOpacity>
+                            <ScrollView
+                                style={{width: screenWidth, height: 10}}
+                                refreshControl={
+                                    <RefreshControl
+                                        style={[styles.centered]}
+                                        {...refreshProps}
+                                    />
+                                }>
+                            </ScrollView>
+                        </View>
+
+                    );
+                }
+            }
+        }
+        return (
+            <ListView
+                {...this.props}
+                style={{flex: 1}}
+                removeClippedSubviews={false}
+                ref="list"
+                enableEmptySections
+                initialListSize={this.props.pageSize}
+                pageSize={this.props.pageSize}
+                onEndReachedThreshold={50}
+                refreshControl={
+                    <RefreshControl
+                        style={[styles.centered]}
+                        {...refreshProps}/>}
+                onEndReached={this._loadMore}
+                renderFooter={this._renderFooter}
+                dataSource={dataList}
+            />
+        )
+    }
+
+    showRefreshState() {
+        this.setState({
+            isRefresh: true,
+        });
+    }
+
+    scrollToTop() {
+        if (this.refs.list)
+            this.refs.list.scrollTo({y: 0, animate: false});
+    }
+
+    refreshComplete(showLoadMore = false, scrollToTop = false) {
+        this.setState({
+            isRefreshing: false,
+            isRefresh: false,
+            showLoadMore: showLoadMore,
+        });
+        if (scrollToTop)
+            this.scrollToTop();
+    }
+
+    loadMoreComplete(showLoadMore = false) {
+        this.setState({
+            isRefreshing: false,
+            showRefresh: true,
+            showLoadMore: showLoadMore,
+        });
+    }
+
+}
+
+PullLoadMoreListView.propTypes = {
+    pageSize: PropTypes.number,
+    dataSource: PropTypes.any,
+    refresh: PropTypes.func,
+    loadMore: PropTypes.func,
+    enableRefresh: PropTypes.bool,
+};
+PullLoadMoreListView.defaultProps = {
+    pageSize: Config.PAGE_SIZE,
+    dataSource: [],
+    enableRefresh: true,
+};
+
+export default PullLoadMoreListView;
